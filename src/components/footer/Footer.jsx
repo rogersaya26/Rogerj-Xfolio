cd1f104ff33ec168b3d9b3f62cc45e5ea264b173
rogersaya26 <rogersaya@gmail.com>
2022-01-31 11:31:50 -0300

    skills tecnologies logos modified

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = ({clickMe, setClickMe}) => {
         <a href="https://drive.google.com/file/d/1EjM9RRUFmznASuX_MuiGtHirH7XA4Zrc/view?usp=sharing" target="_blank" rel="noreferrer" ><p> DESCARGAR CV</p></a>
      
         <a href="https://www.linkedin.com/in/roger-jimenez-3929149b/" target="_blank" rel="noreferrer" > <span className="logo_linkedin"><ion-icon name="logo-linkedin"></ion-icon></span></a>
-        <a href="https://github.com/rogersaya26?tab=repositories" target="_blank" rel="noreferrer" > <span className="logo_github" ><ion-icon name="logo-github"></ion-icon></span></a>
+        <a href="https://github.com/rogersaya26" target="_blank" rel="noreferrer" > <span className="logo_github" ><ion-icon name="logo-github"></ion-icon></span></a>
 
       </StyledContacto>
 
--- src/components/skill/Skill.jsx
+++ src/components/skill/Skill.jsx
@@ -3,26 +3,31 @@ import CssLogo from "../images/css-logo.png";
 import JsLogo from "../images/javascript-logo.png";
 import ReactLogo from "../images/react-logo.png";
 import StyledComponentsLogo from "../images/styled-components-logo.png";
-import SqlLogo from "../images/sql-logo.png";
 import BootstrapLogo from "../images/bootstrap-logo.png";
 import GitLogo from "../images/git-logo.png";
 import JavaLogo from "../images/java-logo.png";
-import SpringBootLogo from "../images/springboot-logo.png"
+import SpringBootLogo from "../images/springboot-logo.png";
+import PostmanLogo from "../images/postman-logo.png";
+import NpmLogo from "../images/npm-logo.png";
+import MySqlLogo from "../images/mysql-logo.png";
+import MavenLogo from "../images/maven-logo.png";
+import SqlServerLogo from "../images/sqlserver-logo.png";
 import "./Skill.css";
-
 
 const Skill = () => {
   return (
-
-
     <div className="skill_container" id="skillid">
       <h1>Skills</h1>
 
       <div className="logo_container">
-      
         <div className="logo">
           <img src={ReactLogo} alt="React.js Logo" />
           <h2>React.Js</h2>
+        </div>
+
+        <div className="logo">
+          <img src={JsLogo} alt="Javascript Logo" />
+          <h2>Javascript</h2>
         </div>
 
         <div className="logo">
@@ -36,17 +41,41 @@ const Skill = () => {
         </div>
 
         <div className="logo">
-          <img src={JsLogo} alt="Javascript Logo" />
-          <h2>Javascript</h2>
+          <img src={StyledComponentsLogo} alt="Styled-Components Logo" />
+          <h2>Styled-Components</h2>
         </div>
 
-        <div className="logo">
-          <img src={StyledComponentsLogo} alt="Styled-Components Logo" />
-          <h2>Styled</h2>
-        </div>
+
+
+
 
         <div className="logo_container">
+          <div className="logo">
+            <img src={BootstrapLogo} alt="Bootstrap Logo" />
+            <h2>Bootstrap</h2>
+          </div>
 
+          <div className="logo">
+            <img src={NpmLogo} alt="Npm Logo" />
+            <h2>Npm</h2>
+          </div>
+
+          
+          <div className="logo">
+            <img src={MavenLogo} alt="Maven Logo" />
+            <h2>Maven</h2>
+          </div>
+
+          <div className="logo">
+            <img src={GitLogo} alt="Git Logo" />
+            <h2>Git</h2>
+          </div>
+        </div>
+
+
+
+
+        <div className="logo_container">
           <div className="logo">
             <img src={JavaLogo} alt="Java Logo" />
             <h2>Java</h2>
@@ -58,18 +87,18 @@ const Skill = () => {
           </div>
 
           <div className="logo">
-            <img src={BootstrapLogo} alt="Bootstrap Logo" />
-            <h2>Bootstrap</h2>
+            <img src={PostmanLogo} alt="Postman Logo" />
+            <h2>Postman</h2>
           </div>
 
           <div className="logo">
-            <img src={SqlLogo} alt="Styled-Components Logo" />
-            <h2>Sql</h2>
+            <img src={SqlServerLogo} alt="SqlServer Logo" />
+            <h2>SqlServer</h2>
           </div>
 
           <div className="logo">
-            <img src={GitLogo} alt="Git Logo" />
-            <h2>Git</h2>
+            <img src={MySqlLogo} alt="MySql Logo" />
+            <h2>MySqL</h2>
           </div>
         </div>
       </div>
